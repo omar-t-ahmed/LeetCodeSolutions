99ecf6e6aae8f7ed23e40ec5ad4d370bd3e1fc8a
omar-t-ahmed <omartahmed@gmail.com>
2024-01-30 15:13:14 -0500

    problem 150

--- solutions.js
+++ solutions.js
@@ -28,3 +28,69 @@ var getConcatenation = function(nums) {
 
     return res
 };
+
+var isValid = function(s) {
+    let chars = s.split()
+
+    for (let i = 0; i < chars.length; i++) {
+        let curr = chars[i]
+        let next = chars[i + 1]
+
+        if (curr === '(') {
+            if (next !== ')') {
+                return false
+            }
+            
+        } else if (curr === '[') {
+            if (next !== ']') {
+                return false
+            }
+
+        } else if (curr === '{') {
+            if (next !== '}') {
+                return false
+            }
+        }
+    }
+
+    return true
+};
+
+isValid("(]")
+
+
+// 150. Evaluate Reverse Polish Notation
+
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+
+// 1. if num push it onto stack
+// 2. if operand 
+
+var evalRPN = function(tokens) {
+    let numStack = []
+    let operators = {'+': (num1, num2) => num1 + num2,
+    '-': (num1, num2) => num1 - num2,
+    '*': (num1, num2) => num1 * num2,
+    '/': (num1, num2) => num1 / num2,
+    }
+
+    tokens.forEach((token) => {
+
+        if (token in operators) {
+            let num2 = numStack.pop()
+            let num1 = numStack.pop()
+
+            let res = Math.trunc(operators[token](num1, num2))
+
+            numStack.push(res)
+        } else {
+            
+            numStack.push(Number(token))
+        }
+    })
+
+    return numStack
+};
